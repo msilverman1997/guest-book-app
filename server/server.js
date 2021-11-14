36b9aa2d5771bf1dc0fa23a9443761a505ac492b
Michael <mhs126@case.edu>
2021-11-14 16:16:23 -0500

    Docker changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require("./models");
 const routes = require('./routes/guest.routes');
-
 
 db.sequelize.sync();
 
